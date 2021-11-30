24f4bdb7cbbd69b77e394b5cb5ae66c5af6c2f55
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-12-01 00:10:06 +0900

    [func] pm2 setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,6 @@ module.exports = {
     {
       name: 'algo-project-server-dev',
       script: 'dist/main.js',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
       instance: 0,
       exec_mode: 'cluster',
       kill_timeout: 5000,
